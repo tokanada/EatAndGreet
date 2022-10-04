051ab43b71ea65f99ef92524e08c46ac8b0841df
tokanada <davidt.3200@gmail.com>
2022-10-03 23:02:20 -0700

    working app

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>
+          HELLO WORLD
+        </h1>
+        <p>
+            EAT AND GREET
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
